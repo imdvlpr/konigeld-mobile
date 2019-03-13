49992d5095724b35664173ba47201e8027b55728
Jonathan Budiman <jojobudiman@gmail.com>
2019-03-13 20:15:01 +0700

    Update interface v.1.2
    Added some charge interfaces and drawables as an additional design

---
+++ app/src/main/java/com/application/jojobudiman/konigeldandroid/ChargePayment.java
@@ -0,0 +1,14 @@
+package com.application.jojobudiman.konigeldandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChargePayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charge_payment);
+    }
+}
---
+++ app/src/main/java/com/application/jojobudiman/konigeldandroid/EnterCash.java
@@ -0,0 +1,14 @@
+package com.application.jojobudiman.konigeldandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterCash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_cash);
+    }
+}
