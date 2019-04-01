f920df6de68b570d22fea15a3c3d00ce6cf52e98
Jonathan Budiman <jojobudiman@gmail.com>
2019-04-01 20:27:33 +0700

    Edit API configuration for sign in (Still error)

--- app/src/main/java/com/application/jojobudiman/konigeldandroid/starter/SignIn.java
+++ app/src/main/java/com/application/jojobudiman/konigeldandroid/starter/SignIn.java
@@ -3,7 +3,9 @@ package com.application.jojobudiman.konigeldandroid.starter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +13,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.application.jojobudiman.konigeldandroid.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class SignIn extends AppCompatActivity {
@@ -29,24 +41,89 @@ public class SignIn extends AppCompatActivity {
         pass = (EditText) findViewById(R.id.passwordText);
         forgotpass = (TextView) findViewById(R.id.forgetpass);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String  semail, spass;
-                semail = email.getText().toString();
-                spass = pass.getText().toString();
 
-                if (semail.length() == 0 || spass.length() == 0) {
-                    Toast.makeText(getApplication(), "All fields must be filled!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void buLogin(View view) {
+        String url="http://10.2.2:8888/semester8/konigeld/assets/mobile/login.php?email_user="+email.getText().toString()+"&pass_user="+pass.getText().toString();
+        new getMySqlData().execute(url);
+        Log.v("myApp", url); //Mengeluarkan di logcat
+        //Toast.makeText(getApplicationContext(), url, Toast.LENGTH_LONG).show();
+    }
+
+    public class getMySqlData extends AsyncTask<String, String, String> {
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                String NewsData;
+                URL url = new URL(params[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setConnectTimeout(7000);
+
+                try {
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    NewsData = ConvertInputToStringNoChange(in);
+                    publishProgress(NewsData);
+                } finally {
+                    urlConnection.disconnect();
                 }
-                else {
-                    Intent i = new Intent(getApplicationContext(), WelcomeText.class);
-                    startActivity(i);
-                }
+
+            }catch (Exception ex){
 
             }
-        });
+            return null;
+        }
+
+
+        protected void onProgressUpdate(String... progress) { //Ambil 1 array yang dilakukan
+
+            try {
+                JSONObject json= new JSONObject(progress[0]);
+                //JSONArray descWeather = json.getJSONArray("user"); //Memanggil JSONArray (Dari array object) dari data
+
+                String id = json.getString("id_user");
+                Toast.makeText(getApplicationContext(), id, Toast.LENGTH_LONG).show();
+
+
+            } catch (Exception ex) {
+
+                Toast.makeText(getApplicationContext(),"Fail load API data",Toast.LENGTH_LONG).show();
+            }
+
+
+        }
+
+        protected void onPostExecute(String  result2){
+
+
+        }
 
 
     }
+
+
+    public static String ConvertInputToStringNoChange(InputStream inputStream) { //Menghilangkan string BufferReader
+
+        BufferedReader bureader=new BufferedReader( new InputStreamReader(inputStream));
+        String line ;
+        String linereultcal="";
+
+        try{
+            while((line=bureader.readLine())!=null) {
+                linereultcal+=line;
+
+            }
+            inputStream.close();
+
+
+        }catch (Exception ex){}
+
+        return linereultcal;
+    }
+
+
+
 }
